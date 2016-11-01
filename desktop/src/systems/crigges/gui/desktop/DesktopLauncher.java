c69545f920bcc61efa7768959a9e2b38afdb45bf
unknown <andy400@web.de>
2016-11-01 12:48:46 +0100

    work on multi monitor support

--- desktop/src/systems/crigges/gui/desktop/DesktopLauncher.java
+++ desktop/src/systems/crigges/gui/desktop/DesktopLauncher.java
@@ -8,12 +8,10 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import systems.crigges.gui.Ticker;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		Monitor[] monitors = config.getMonitors();
-		for (int i = 0; i < monitors.length; i++) {
-			System.out.println(monitors[i]);
-		}
-		new Lwjgl3Application(new Ticker(), config);
-	}
+//	Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+//	Monitor[] monitors = config.getMonitors();
+//	for (int i = 0; i < monitors.length; i++) {
+//		System.out.println(monitors[i]);
+//	}
+//	new Lwjgl3Application(new Ticker(), config);
 }
--- desktop/src/systems/crigges/gui/desktop/SortingTable.java
+++ desktop/src/systems/crigges/gui/desktop/SortingTable.java
@@ -22,6 +22,13 @@ import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import javax.swing.JSpinner;
 import javax.swing.border.LineBorder;
+
+import com.badlogic.gdx.Graphics.Monitor;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+import systems.crigges.gui.Ticker;
+
 import java.awt.Color;
 import javax.swing.DefaultComboBoxModel;
 
@@ -34,6 +41,8 @@ public class SortingTable {
 	private JSpinner ringSpinner;
 	private ArrayList<String> classes;
 	private JComboBox<Object> classSelector;
+	private JComboBox<String> deviceBox;
+	private Monitor[] monitors;
 
 	/**
 	 * Launch the application.
@@ -135,24 +144,53 @@ public class SortingTable {
 			}
 		});
 		
-		classSelector = new JComboBox();
+		classSelector = new JComboBox<Object>();
+		
+		deviceBox = new JComboBox<String>();
+		
+		
+		
+		JLabel lblTargetDevice = new JLabel("Target Device");
+		
+		JButton btnNewButton_1 = new JButton("Start");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Thread starter = new Thread(new Runnable() {
+					
+					@Override
+					public void run() {
+						for(Monitor m : monitors){
+							if(deviceBox.getSelectedItem().toString().equals(m.name)){
+								Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+								config.setFullscreenMode(config.getDisplayMode(m));
+								new Lwjgl3Application(new Ticker(), config);
+							}
+						}
+					}
+				});
+				starter.start();
+			}
+		});
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblName)
-						.addComponent(lblNewLabel)
-						.addComponent(lblKlasse)
-						.addComponent(ringSpinner, GroupLayout.DEFAULT_SIZE, 253, Short.MAX_VALUE)
-						.addComponent(btnEintragHinzufgen, GroupLayout.PREFERRED_SIZE, 253, Short.MAX_VALUE)
-						.addComponent(btnLschen, GroupLayout.DEFAULT_SIZE, 253, Short.MAX_VALUE)
+						.addComponent(ringSpinner, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 253, Short.MAX_VALUE)
+						.addComponent(btnEintragHinzufgen, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 253, Short.MAX_VALUE)
+						.addComponent(btnLschen, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 253, Short.MAX_VALUE)
 						.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
 							.addComponent(classSelector, 0, 203, Short.MAX_VALUE)
 							.addPreferredGap(ComponentPlacement.RELATED)
 							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE))
-						.addComponent(nameField, GroupLayout.DEFAULT_SIZE, 253, Short.MAX_VALUE))
+						.addComponent(nameField, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 253, Short.MAX_VALUE)
+						.addComponent(lblName)
+						.addComponent(lblKlasse)
+						.addComponent(lblNewLabel)
+						.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, 253, Short.MAX_VALUE)
+						.addComponent(deviceBox, 0, 253, Short.MAX_VALUE)
+						.addComponent(lblTargetDevice))
 					.addContainerGap())
 		);
 		gl_panel.setVerticalGroup(
@@ -162,9 +200,9 @@ public class SortingTable {
 					.addComponent(lblName)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(nameField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(16)
+					.addGap(14)
 					.addComponent(lblKlasse)
-					.addGap(7)
+					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 						.addComponent(btnNewButton)
 						.addComponent(classSelector, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
@@ -176,7 +214,13 @@ public class SortingTable {
 					.addComponent(btnEintragHinzufgen)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(btnLschen)
-					.addContainerGap(333, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.RELATED, 224, Short.MAX_VALUE)
+					.addComponent(lblTargetDevice)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(deviceBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnNewButton_1)
+					.addContainerGap())
 		);
 		panel.setLayout(gl_panel);
 		
@@ -185,5 +229,31 @@ public class SortingTable {
 		table.setModel(tableModel);
 		scrollPane.setViewportView(table);
 		frame.getContentPane().setLayout(groupLayout);
+		Thread monitorScaner = new Thread(new MonitorScanTask());
+		monitorScaner.start();
+	}
+	
+	public class MonitorScanTask implements Runnable {
+		
+		private String[] monitorNames = {};
+
+		public void run() {
+			while(true){
+				monitors = Lwjgl3ApplicationConfiguration.getMonitors();
+				if(monitors.length > monitorNames.length){
+					monitorNames = new String[monitors.length];
+					for (int i = 0; i < monitors.length; i++) {
+						monitorNames[i] = monitors[i].name;
+					}
+					deviceBox.setModel(new DefaultComboBoxModel<String>(monitorNames));
+				}
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
